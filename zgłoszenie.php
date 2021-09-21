1c0d21786d0f99ba6972bdb3c34911be74608a2b
Komandes <72608602+Komandes@users.noreply.github.com>
2021-09-21 13:42:03 +0200

    Initial commit

---
+++ zgłoszenie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie - moje hobby</title>
+</head>
+<body>
+    <?php
+        $łowisko = $_POST['łowisko'];
+        $data = $_POST['data'];
+        $sędzia = $_POST['sędzia'];
+
+        $db = new mysql('localhost', 'root', '', 'egzamin2')
+    ?>
+</body>
+</html>
